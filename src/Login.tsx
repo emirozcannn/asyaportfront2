9a64f9c0ad082c4d6f054d30fa1d064316e71a3c
Furkan Arslan <2200656003nku.edu.tr>
2025-08-08 13:38:00 +0300

    furkan

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { loginUser } from './api2/supabaseAuth';
 import { useNavigate } from 'react-router-dom';
-
+//furkan
 const Login: React.FC = () => {
   const [email, setEmail] = useState('bt.mudur@asyaport.com');
   const [password, setPassword] = useState('alper1emir');
